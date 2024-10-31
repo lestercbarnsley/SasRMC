985dcc0fa1824d5e0678e99d243d250d3498652b
lestercbarnsley <lester.barnsley@gmail.com>
2024-10-31 21:59:02 +1100

    need to review profile structure calculator

--- sas_rmc/result_calculator.py
+++ sas_rmc/result_calculator.py
@@ -4,7 +4,8 @@ from dataclasses import dataclass
 
 import numpy as np
 
-from sas_rmc import constants, vector, Particle
+from sas_rmc import constants, vector
+from sas_rmc.box_simulation import Box
 from sas_rmc.array_cache import method_array_cache
 from sas_rmc.detector import Polarization
 from sas_rmc.form_calculator import box_intensity, FieldDirection
@@ -22,6 +23,7 @@ class ResultCalculator(ABC):
     def intensity_result(self, scattering_simulation: ScatteringSimulation) -> np.ndarray:
         pass
 
+
 def modulated_form_array(particle: ParticleForm, qx_array: np.ndarray, qy_array: np.ndarray) -> FormResult:
     form_result = particle.form_result(qx_array, qy_array)
     position = particle.get_position()
@@ -33,6 +35,9 @@ def modulated_form_array(particle: ParticleForm, qx_array: np.ndarray, qy_array:
         form_magnetic_z=form_result.form_magnetic_z * modulation
     )
 
+def particle_forms_from(box: Box) -> list[ParticleForm]:
+    return [particle for particle in box.particles if isinstance(particle, ParticleForm)]
+
 
 @dataclass
 class AnalyticalCalculator(ResultCalculator):
@@ -42,9 +47,7 @@ class AnalyticalCalculator(ResultCalculator):
     field_direction: FieldDirection = FieldDirection.Y
 
     @method_array_cache(cache_holder_index=1)
-    def modulated_form_array(self, particle: Particle) -> FormResult:
-        if not isinstance(particle, ParticleForm):
-            raise TypeError("This type of calculator is only compatible with a ParticleForm abstract base class")
+    def modulated_form_array(self, particle: ParticleForm) -> FormResult:
         return modulated_form_array(
             particle=particle,
             qx_array=self.qx_array,
@@ -54,7 +57,7 @@ class AnalyticalCalculator(ResultCalculator):
     def intensity_result(self, scattering_simulation: ScatteringSimulation) -> np.ndarray:
         return np.average(
             [box_intensity(
-                form_results=[self.modulated_form_array(particle) for particle in box.particles], 
+                form_results=[self.modulated_form_array(particle) for particle in particle_forms_from(box)], 
                 box_volume= box.volume, 
                 qx=self.qx_array, 
                 qy=self.qy_array, 
