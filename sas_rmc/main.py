1fd0bf2744ff4a185f60658e41a8d8644eb8aea9
Lester Barnsley <lester.barnsley@gmail.com>
2024-11-17 21:54:05 +1100

    templates should just be downloadable

--- sas_rmc/main.py
+++ sas_rmc/main.py
@@ -13,6 +13,7 @@ with open(CONFIG_FILE) as f:
 
 DEFAULT_INPUT = Path(current_config.get('input_config_source'))
 DEFAULT_OUTPUT = Path(current_config.get('output_folder'))
+DEFAULT_TEMPLATE_SOURCE = current_config.get('template_source')
 
 
 @click.group()
@@ -54,15 +55,20 @@ def show():
 
     show_settings(current_config)
 
+
 @config.command()
 @click.option("-i", "--input", "input", type=click.Path(), help="Update the default .xlsx file to run when otherwise unspecified.", default = DEFAULT_INPUT, show_default = True)
 @click.option("-o", "--output", "output", type = click.Path(), help="Update the default folder where the output is written.", default = DEFAULT_OUTPUT, show_default = True)
-def update(input: Path, output: Path):
+@click.option("-t", "--template", "template", type = click.STRING, help="Update the repository for downloading template files.", default = DEFAULT_TEMPLATE_SOURCE, show_default = True)
+@click.confirmation_option(prompt='Are you sure you want to update the config?')
+def update(input: Path, output: Path, template: str):
     """Update the current configuration."""
     if input != DEFAULT_INPUT:
         current_config["input_config_source"] = str(input)
     if output != DEFAULT_OUTPUT:
         current_config["output_folder"] = str(output)
+    if template != DEFAULT_TEMPLATE_SOURCE:
+        current_config["template_source"] = str(output)
 
     with open(CONFIG_FILE, 'w') as f:
         yaml.dump(current_config, f, Dumper = yaml.Dumper)
@@ -70,6 +76,17 @@ def update(input: Path, output: Path):
     click.echo(f"Config updated.")
 
     show_settings(current_config)
+
+
+@click.group()
+@click.argument('template-type', type=click.Choice(['coreshell', 'dumbbell'], case_sensitive=False))
+@click.option("-o", "--output", "output", help="Folder to output results.", type = click.Path(), default = DEFAULT_OUTPUT, show_default = True)
+def create(template_type):
+    """Configuration options."""
+    if template_type == 'coreshell':
+        pass
+
+#@
 
 
 cli.add_command(run)
