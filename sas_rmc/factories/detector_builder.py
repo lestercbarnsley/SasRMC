5b3cf31cfbf3372caf728476d952539dc9901efe
Lester Barnsley <lester.barnsley@ansto.gov.au>
2024-11-22 17:59:52 +1100

    turning detector config abc so i can have two different implementations for smearing and non smearing

--- sas_rmc/factories/detector_builder.py
+++ sas_rmc/factories/detector_builder.py
@@ -1,6 +1,6 @@
 #%%
 
-
+from abc import ABC, abstractmethod
 from collections.abc import Callable
 import pandas as pd
 from pydantic.dataclasses import dataclass as pydantic_dataclass
@@ -28,6 +28,24 @@ class DetectorPixelFactory:
     sigma_perp: float | None = None
     shadow_factor: bool = True
 
+    def create_pixel(self, sigma_para_derived: float | None = None, sigma_perp_derived: float | None = None) -> DetectorPixel:
+        sigma_para = self.sigma_para if self.sigma_para is not None else sigma_para_derived # Prefer to get the pixel data from self, injection is the second choice
+        sigma_perp = self.sigma_perp if self.sigma_perp is not None else sigma_perp_derived
+        if sigma_perp is None:
+            raise ValueError("Missing data for sigma perp")
+        if sigma_para is None:
+            raise ValueError("Missing data for sigma para")
+        return DetectorPixel(
+            qX=self.qX,
+            qY=self.qY,
+            intensity=self.intensity,
+            intensity_err=self.intensity_err,
+            qZ=self.qZ,
+            sigma_para=sigma_para,
+            sigma_perp=sigma_perp,
+            shadow_factor = self.shadow_factor
+        )
+
     @classmethod
     def gen_from_row(cls, row: dict):
         d = row | {
@@ -37,7 +55,19 @@ class DetectorPixelFactory:
     
 
 @pydantic_dataclass
-class DetectorConfig:
+class DetectorConfig(ABC):
+
+    @abstractmethod
+    def get_polarization(self) -> Polarization:
+        pass
+
+    @abstractmethod
+    def create_pixel(self, pixel_factory: DetectorPixelFactory) -> DetectorPixel:
+        pass
+    
+
+@pydantic_dataclass
+class DetectorConfigSmearing(DetectorConfig):
     detector_distance_in_m: float
     collimation_distance_in_m: float
     collimation_aperture_area_in_m2: float
@@ -63,16 +93,13 @@ class DetectorConfig:
         sigma_para = np.sqrt((q * (self.wavelength_spread / 2))**2 + sigma_geom**2)
         return sigma_para
     
+    def get_polarization(self) -> Polarization:
+        return self.polarization
+    
     def create_pixel(self, pixel_factory: DetectorPixelFactory) -> DetectorPixel: # The pixel factory coerces types as it is a Pydantic dataclass
-        return DetectorPixel(
-            qX=pixel_factory.qX,
-            qY=pixel_factory.qY,
-            intensity=pixel_factory.intensity,
-            intensity_err=pixel_factory.intensity_err,
-            qZ=pixel_factory.qZ,
-            sigma_para=pixel_factory.sigma_para if pixel_factory.sigma_para is not None else self.get_sigma_parallel(pixel_factory.qX, pixel_factory.qY),
-            sigma_perp=pixel_factory.sigma_perp if pixel_factory.sigma_perp is not None else self.get_sigma_geometric(),
-            shadow_factor = pixel_factory.shadow_factor
+        return pixel_factory.create_pixel(
+            sigma_para_derived=self.get_sigma_parallel(pixel_factory.qX, pixel_factory.qY),
+            sigma_perp_derived=self.get_sigma_geometric()
         )
     
     @classmethod
@@ -96,36 +123,55 @@ class DetectorConfig:
 
 @pydantic_dataclass
 class DetectorConfigNoSmear(DetectorConfig):
-    def get_sigma_geometric(self) -> float:
-        return 0.0
-    def get_sigma_parallel(self, qx: float, qy: float) -> float:
-        return 0.0
+    polarization: Polarization = Polarization.UNPOLARIZED
+
+    def get_polarization(self) -> Polarization:
+        return self.polarization
+    
+    def create_pixel(self, pixel_factory: DetectorPixelFactory) -> DetectorPixel:
+        return pixel_factory.create_pixel(
+            sigma_para_derived=0.0,
+            sigma_perp_derived=0.0
+        )
+    
+    @classmethod
+    def gen_from_config(cls, config: dict):
+        d = config | {
+            'polarization' : config.get('Polarization', Polarization.UNPOLARIZED.value)
+        }
+        if d['polarization'] == 'down':
+            d['polarization'] = 'spin_down'
+        elif d['polarization'] == 'up':
+            d['polarization'] = 'spin_up'
+        return cls(**d)
     
 
-def detector_image_from_dataframe(detector_df: pd.DataFrame, detector_config: DetectorConfig, polarization: Polarization | None = None):
-    pixels = [detector_config.create_pixel(DetectorPixelFactory.gen_from_row({k : v for k, v in row.items()})) for _, row in detector_df.iterrows()]
-    return DetectorImage(pixels, polarization=polarization if polarization is not None else detector_config.polarization)
+def detector_image_from(dataframe: pd.DataFrame, detector_config: DetectorConfig, polarization: Polarization | None = None) -> DetectorImage:
+    pixels = [detector_config.create_pixel(DetectorPixelFactory.gen_from_row({k : v for k, v in row.items()})) for _, row in dataframe.iterrows()]
+    return DetectorImage(pixels, polarization=polarization if polarization is not None else detector_config.get_polarization())
 
-def create_detector_image(dataframes: dict[str, pd.DataFrame], detector_config: DetectorConfig, buffer_source: str | None = None) -> DetectorImage: # raises KeyError
-    detector_image = detector_image_from_dataframe(dataframes["Data Source"], detector_config)
+def create_detector_image(dataframes: dict[str, pd.DataFrame], detector_config: DetectorConfig, data_source: str, buffer_source: str | None = None) -> DetectorImage: # raises KeyError
+    detector_image = detector_image_from(dataframes[data_source], detector_config)
     if not buffer_source:
         return detector_image
     if isinstance(buffer_source, float):
         return subtract_detectors(detector_image, buffer_source)
     if buffer_source in dataframes:
-        buffer_image = detector_image_from_dataframe(dataframes[buffer_source], detector_config, Polarization.UNPOLARIZED)
+        buffer_image = detector_image_from(dataframes[buffer_source], detector_config, Polarization.UNPOLARIZED)
         return subtract_detectors(detector_image, buffer_image)
     raise KeyError("Could not find buffer source")
     
 def create_detector_images(dataframes: dict[str, pd.DataFrame], detector_config_creator: Callable[[dict], DetectorConfig]) -> list[DetectorImage]:
     value_frame = list(dataframes.values())[0]
     value_dict = parse_data.parse_value_frame(value_frame)
-    if value_dict.get("Data Source"):
-        return [create_detector_image(dataframes, detector_config_creator(value_dict), buffer_source=value_dict.get('Buffer Source'))]
+    data_source = value_dict.get("Data Source")
+    if data_source:
+        return [create_detector_image(dataframes, detector_config_creator(value_dict), data_source = data_source, buffer_source=value_dict.get('Buffer Source'))]
     df = dataframes['Data parameters']
     return [create_detector_image(
         dataframes=dataframes,
         detector_config=detector_config_creator({k : v for k, v in row.items()}),
+        data_source = row['Data Source'], # This will raise a Key Error
         buffer_source=row.get('Buffer Source')
     ) for _, row in df.iterrows()]
 
@@ -133,36 +179,11 @@ def create_detector_images_no_smearing(dataframes: dict[str, pd.DataFrame]) -> l
     return create_detector_images(dataframes, DetectorConfigNoSmear.gen_from_config)
 
 def create_detector_images_with_smearing(dataframes: dict[str, pd.DataFrame]) -> list[DetectorImage]:
-    return create_detector_images(dataframes, DetectorConfig.gen_from_config)
+    return create_detector_images(dataframes, DetectorConfigSmearing.gen_from_config)
     
     
 if __name__ == "__main__":
-    import inspect
-    from pprint import pprint
-    ### I'm keeping this here for now, because this could be a useful way to propagate
-    ### Config parameters to multiple factories
-
-
-    def coerce_types(func):
-
-        def wrapper(*args, **kwargs):
-
-            coerced_kwargs = {}
-            for k, v in inspect.signature(func).parameters.items():
-                if k in kwargs:
-                    coerced_kwargs[k] = v.annotation(kwargs[k])
-            return func(*args, **coerced_kwargs)
-        return wrapper
-    
-    @coerce_types
-    def test_1(a: float, b: float, c: float) -> float:
-        return a + b + c
-    
-    print(test_1(**{'a' : '3.7', 'b' : '4', 'c': 7, 'd' : 8}))
-
-    @coerce_types
-    def test_with_polarization(polarization: Polarization) -> None:
-        print(polarization)
+    pass
 
 
 
