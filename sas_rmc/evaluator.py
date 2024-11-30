found a major bug in how shadow factor was masking

sas_rmc/evaluator.py
@@ -51,8 +51,12 @@ class Fitter(ABC):
 
 def calculate_goodness_of_fit(simulated_intensity: np.ndarray, experimental_detector: DetectorImage) -> float:
     experimental_intensity = experimental_detector.intensity
-    uncertainty = np.where(experimental_detector.intensity_err == 0, 1, experimental_detector.intensity_err)
-    difference_of_squares =((experimental_intensity - simulated_intensity)**2 / uncertainty**2)[experimental_detector.shadow_factor]
+    uncertainty = np.where(
+        experimental_detector.intensity_err == 0, 
+        1, 
+        experimental_detector.intensity_err
+        )
+    difference_of_squares =((experimental_intensity - simulated_intensity)**2 / uncertainty**2)[experimental_detector.shadow_factor.nonzero()]
     return np.average(difference_of_squares).item() # Use np.average here because the constant function takes a list
 
 def qXqY_delta(detector: DetectorImage) -> tuple[float, float]: