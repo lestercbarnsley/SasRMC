9599a29be1bd7f2db50e585ef319bf1ec7ba3fe4
lestercbarnsley <lester.barnsley@gmail.com>
2024-12-24 16:42:41 +1000

    failed attempt to make a validator dataclass work

--- sas_rmc/factories/parse_data.py
+++ sas_rmc/factories/parse_data.py
@@ -32,3 +32,97 @@ def coerce_types(func):
             return func(*args, **coerced_kwargs)
         return wrapper
 
+if __name__ == "__main__":
+
+    def validate_bool(s: str | float | int) -> bool:
+        if s == 1:
+            return True
+        if s == 0:
+            return False
+        if isinstance(s, int | float):
+            return bool(s)
+        if s.lower() == "true":
+            return True
+        if s.lower() == 'false':
+            return False
+        if s.lower() == "on":
+            return True
+        if s.lower() == "off":
+            return False
+        if s.lower() == 'y' or 'yes' in s.lower():
+            return True
+        if s.lower() == 'n' or 'no' in s.lower():
+            return False
+        return bool(s)
+
+    from dataclasses import dataclass
+
+    from functools import wraps
+
+    from collections.abc import Callable
+
+    from typing import ParamSpec, TypeVar
+
+    T = TypeVar("T", bound=type)
+
+    def validated_dataclass(cls: T) -> T:
+        dclas = dataclass(cls)
+        print(dclas.__name__)
+
+        '''@wraps(dclas)
+        def f(*args, **kwargs):
+            inst = dclas(*args, **kwargs)
+            for name, field in inst.__dataclass_fields__.items():
+                t = field.type
+                value = getattr(inst, name)
+                if isinstance(value, t):
+                    continue
+                try:
+                    if t == bool:
+                        setattr(inst, name, validate_bool(value))
+                        continue
+                    setattr(inst, name, t(value))
+                except Exception:
+                    raise
+            return inst
+        return f'''
+                
+
+
+
+        @dataclass
+        class ValidatedClass(dclas):
+
+            def __setattr__(self, name: str, value) -> None:
+                field = self.__dataclass_fields__.get(name)
+                if field is not None:
+                    t = field.type
+                    if isinstance(value, t):
+                        return super().__setattr__(name, value)
+                    
+                    try:
+                        if t == bool:
+                            return super().__setattr__(name, validate_bool(value))
+                        return super().__setattr__(name, t(value))
+                    except Exception:
+                        raise
+
+        return ValidatedClass
+    
+   
+
+    @validated_dataclass
+    @dataclass
+    class Test:
+        x: float
+        y: float
+        t: bool
+
+    t = Test(3, 4, t ='yes')
+    print(t)
+    print(t.t)
+    
+    
+
+
+#%%
